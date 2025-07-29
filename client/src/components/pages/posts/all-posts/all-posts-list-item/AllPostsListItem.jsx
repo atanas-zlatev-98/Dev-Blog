151fcc163b9d47588bf43e76850c4b3492f5dd80
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2025-07-29 19:24:19 +0300

    improved all posts list components style

--- client/src/components/pages/posts/all-posts/all-posts-list-item/AllPostsListItem.jsx
+++ client/src/components/pages/posts/all-posts/all-posts-list-item/AllPostsListItem.jsx
@@ -1,13 +1,47 @@
 import React, { useEffect, useState } from "react";
 import "./AllPostsListItem.style.scss";
 import { NavLink } from "react-router";
+import moment from 'moment';
 
-const AllPostsListItem = ({ _id,title, content, imageUrl }) => {
+const initialState = {
+  username: "",
+  profilePicture: "",
+};
+
+const AllPostsListItem = ({_id,title,author,createdAt,tags,comments}) => {
+  const [postAuthor, setPostAuthor] = useState(initialState);
+
+  const formattedDate = moment(createdAt).utc().format('DD/MMMM');
+
   return (
     <NavLink to={`/posts/${_id}`} className="mini-post-container">
-      <div >
-        <p>{title}</p>
-        <p>{content}</p>
+      <div className="post">
+        <div className="author-data">
+          <div className="author">
+            <img className="author-img" src="https://d4804za1f1gw.cloudfront.net/wp-content/uploads/sites/130/2024/10/terminator_dark_fate___2019_new_movie_trailer_by_epicheroes_dd7w5hm-fullview-444x444.jpg"></img>
+            <div>
+              <p>Author</p>
+              <p>{formattedDate}</p>
+            </div>
+          </div>
+         
+        </div>
+        <div className="post-data">
+          <div className="post-title">
+            <h2>{title}</h2>
+          </div>
+          <div className="post-tags">
+           
+            {tags.map((tag) => (
+              <p>#{tag}</p>
+            ))}
+          </div>
+          <div className="post-reactions">
+            <div>{/* TODO REACTIONS */}</div>
+            <div>Comments {comments.length}</div>
+            <div>Save Post</div>
+          </div>
+        </div>
       </div>
     </NavLink>
   );
